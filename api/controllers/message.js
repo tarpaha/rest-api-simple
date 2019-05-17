15e146ea58704d7930e2fcb5799fdeea4ca92077
tarpaha <ppopchikovsky@gmail.com>
2019-05-17 21:06:41 +0300

    tiny log changes in message controller

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -6,7 +6,7 @@ var Message = require('mongoose').model('Message');
 module.exports.list = function(req, res) {
     Message.find(function(err, messages) {
         if(err) {
-            logger.error('messages.list() error: ' + err);
+            logger.error('Message.find() error: ' + err.message);
             res.statusCode = 500;
             return res.send({ error: 'Server error' });
         }
@@ -20,10 +20,11 @@ module.exports.add = function(req, res) {
     });
     message.save(function(err) {
         if(err) {
-            logger.error('messages.add() error: ' + err);
+            logger.error('Message.add() error: ' + err);
             res.statusCode = 500;
             return res.send({ error: 'Server error' })
         }
+        logger.info('Message added');
         return res.send({ status: 'OK', message: message });
     });
 }
